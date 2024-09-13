b77afae6a69b42b40f66ca0403b3eeb2eedd78ac
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2024-09-13 16:06:53 -0300

    Update dependency.ts

--- src/misc/dependency.ts
+++ src/misc/dependency.ts
@@ -1,3 +1,4 @@
+import { Handler } from "../net/handler";
 import { Manager } from "../net/manager";
 import { Setup } from "../net/setup";
 import { serviceLocator } from "./service-locator";
@@ -6,6 +7,6 @@ export class Dependency {
   public setup() {
     serviceLocator.registerFactory<Setup>(Setup, () => new Setup());
     serviceLocator.registerFactory<Manager>(Manager, () => new Manager());
-    
+    serviceLocator.registerFactory<Handler>(Handler, () => new Handler());
   }
 }
